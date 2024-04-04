68ac4ac25e546a4951076843cc86e2e2776e028c
Theabergnord <thea.bergnord@hotmail.com>
2024-04-04 20:58:27 +0200

    Setter opp scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+//import './App.css'
+import './css/main.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Searchresults from './components/Searchresults'
 import Layout from './components/Layout'
